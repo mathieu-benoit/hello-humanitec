304e10fced62b1540955045d13038ba1c3885b96
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-08-07 11:59:52 +0000

    TF random provider

--- terraform/azure-storage/providers.tf
+++ terraform/azure-storage/providers.tf
@@ -2,7 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.67.0"
+      version = "~>3.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
 
