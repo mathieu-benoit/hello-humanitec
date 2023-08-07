1ee17d6f9bdb4285988936ac18374d0e9f4a5328
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-08-07 12:37:25 +0000

    terraform/azure-mysql

---
+++ terraform/azure-mysql/main.tf
@@ -0,0 +1,55 @@
+resource "random_string" "server_name" {
+  length  = 8
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "random_string" "database_name" {
+  length  = 8
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "random_string" "login" {
+  length  = 8
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "random_password" "password" {
+  length           = 8
+  lower            = true
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+  min_upper        = 1
+  numeric          = true
+  override_special = "_"
+  special          = true
+  upper            = true
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mysql_flexible_server
+resource "azurerm_mysql_flexible_server" "server" {
+  location                     = var.mysql_server_location
+  name                         = "mysqlserver-${random_string.server_name.result}"
+  resource_group_name          = var.resource_group_name
+  administrator_login          = random_string.login.result
+  administrator_password       = random_password.password.result
+  version                      = "8.0.21"
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mysql_flexible_database
+resource "azurerm_mysql_flexible_database" "database" {
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+  name                = "mysqldatabase_${random_string.database_name.result}"
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.server.name
+}
---
+++ terraform/azure-mysql/variables.tf
@@ -0,0 +1,21 @@
+variable "credentials" {
+  description = "The credentials for connecting to Azure."
+  type = object({
+    azure_subscription_id         = string
+    azure_subscription_tenant_id  = string
+    service_principal_id          = string
+    service_principal_password    = string
+  })
+  sensitive = true
+}
+
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group to use"
+  type        = string
+}
+
+variable "mysql_server_location" {
+  description = "Location of Azure Storage Account"
+  type        = string
+  default     = "eastus"
+}
