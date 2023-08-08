e7d26439dca2902fa5a910969137fb01435416f1
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-08-08 09:27:39 -0400

    Update outputs.tf - account

--- terraform/azure-storage/outputs.tf
+++ terraform/azure-storage/outputs.tf
@@ -2,6 +2,6 @@ output "container" {
   value = azurerm_storage_container.storage_container.name
 }
 
-output "storage_account_name" {
+output "account" {
   value = azurerm_storage_account.storage_account.name
 }
